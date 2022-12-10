c2c590f9b65f85c33a3b652be6b4596aef5e4cf0
YutaYoshimoto <ht18a111@oecu.jp>
2022-12-11 05:20:36 +0900

    アカウント連携前

--- calenders/models.py
+++ calenders/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 class NippoModel(models.Model):
-    #date = models.DateField('日付', blank=True, null=True,default=timezone.now)#年月日、初期値入力日
-    title = models.CharField(max_length=100,default='default-title')#タイトル
+    date = models.DateField(verbose_name='日付', blank=True, null=True,default=timezone.now)#年月日、初期値入力日
+    title = models.CharField(verbose_name='タイトル',max_length=100,default='default-title')#タイトル
     #content = models.CharField(max_length=1000,default='default-contet')
-    content = models.TextField( verbose_name='default-contet')#内容
+    content = models.TextField(verbose_name='内容',default='default-content')#内容
     timestamp = models.DateTimeField(auto_now_add=True)
-    
-    #public = models.BooleanField(default=False, verbose_name="公開する")
-    #slug = models.SlugField(max_length=20, unique=True, default=slug_maker)
+    public = models.BooleanField(default=False, verbose_name="公開する")
+    #slug = models.SlugField(null=True, blank=True)
     
     def __str__(self):
         return self.title
--- calenders/urls.py
+++ calenders/urls.py
@@ -1,18 +1,13 @@
 from django.urls import path
-from .views import nippoListView , nippoDetailView, nippoCreateFormView, nippoUpdateFormView, nippoDeleteView, ImageUploadView#ポイント1
-from .views import NippoListView, NippoCreateFormView, NippoDeleteView#, NippoCreateView
+from .views import ImageUploadView
+from .views import NippoListView, NippoCreateFormView, NippoDeleteView,NippoUpdateFormView ,NippoDetailView
  
  
 urlpatterns = [
   path("", NippoListView.as_view(), name="nippo-list"),
-  
-  path("detail/<int:pk>", nippoDetailView, name="nippo-detail"),
-  
+  path("detail/<int:pk>", NippoDetailView.as_view(), name="nippo-detail"),
   path("create/", NippoCreateFormView.as_view(),name="nippo-create"),
-  
-  path("update/<int:pk>", nippoUpdateFormView, name="nippo-update"),
-  
-  path("delete/<int:pk>/", nippoDeleteView, name="nippo-delete"),
+  path("update/<int:pk>/", NippoUpdateFormView.as_view(), name="nippo-update"),
   path("delete/<int:pk>/", NippoDeleteView.as_view(), name="nippo-delete"),
   path("image-upload/", ImageUploadView.as_view(), name="image-upload")
   #path("", nippoListView,name="nippo-list"), #クラス作成前の関数版
